06c9b0b97269ea304b9b5145926cab1e469689b3
ssafyWooJeong <136890801+ssafyWooJeong@users.noreply.github.com>
2023-09-18 08:48:45 +0900

    Refactor

--- WooJeong/refactor/Torch_QParam_Exporter.py
+++ WooJeong/refactor/Torch_QParam_Exporter.py
@@ -4,13 +4,14 @@ import torch
 import torch.nn
 import torch.quantization
 
-from Torch2CircleMapper import Torch2CircleMapper
+from Torch_Circle_Mapper import Torch2CircleMapper
 from TorchExtractor import TorchExtractor
 
 
 # Helper class of PyTorch Quantization Parameter Export
 class TorchQParamExporter:
-    def __init__(self, original_model: torch.nn.Module, quantized_model: torch.nn.Module,
+    @staticmethod
+    def export(original_model: torch.nn.Module, quantized_model: torch.nn.Module,
                  sample_input: torch.tensor, json_path: str, tflite2circle_path='./tflite2circle'):
         if quantized_model is None or not isinstance(original_model, torch.nn.Module):
             raise Exception("There is no Pytorch Model")
@@ -28,12 +29,9 @@ class TorchQParamExporter:
             dir_path = ""
         else:
             dir_path = json_path[:idx + 1]
-
         mapper = Torch2CircleMapper(original_model=original_model, sample_input=sample_input,
                                     dir_path=dir_path, tflite2circle_path=tflite2circle_path)
-        self.__mapping, partial_graph_data = mapper.get_mapped_dict()
-        self.__extractor = TorchExtractor(quantized_model=quantized_model, json_path=json_path,
+        mapping, partial_graph_data = mapper.get_mapped_dict()
+        extractor = TorchExtractor(quantized_model=quantized_model, json_path=json_path,
                                           partial_graph_data=partial_graph_data)
-
-    def save(self):
-        self.__extractor.generate_files(self.__mapping)
+        extractor.generate_files(mapping)
